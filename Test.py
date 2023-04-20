302f7f32366856081418ddf2896ec81d8703b598
Willson9000 <131329765+Willson9000@users.noreply.github.com>
2023-04-20 13:59:04 +1000

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header:
+    st.title("Wine Quality Dataset analasis")
